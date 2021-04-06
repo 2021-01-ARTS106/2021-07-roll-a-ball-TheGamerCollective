65aae13e49aaddb104dbeffc02ce263a6f7f7e7c
TheGamerCollective <78000191+TheGamerCollective@users.noreply.github.com>
2021-04-05 23:33:09 -0400

    test

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 0f;
+    public TextMeshProUGUI countText;
+    public GameObject winTextObject;
+    public int winCount = 12;
+    private Rigidbody rb;
+    private int count;
+    private float movementX;
+    private float movementY;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        SetCountText();
+
+        winTextObject.SetActive(false);
+    }
+
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movementVector = new Vector3(movementX, 0.0f, movementY);
+
+        rb.AddForce(movementVector * speed);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("PickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+        }
+        SetCountText();
+    }
+
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+
+        if (count >= winCount)
+        {
+            winTextObject.SetActive(true);
+        }
+    }
+}
